585558e7a823e722b8406b4b9877fce78dae68e4
Drinnn <pedro_lontro@hotmail.com>
2022-05-23 09:21:35 -0300

    test: add unique entity id VO tests

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidUuidError } from "../errors/invalid-uuid.error";
+import UniqueEntityId from "./unique-entity-id.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("UniqueEntityId Unit Tests", () => {
+  it("should throw error when uuid is invalid", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a uuid passed in constructor", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const uuid = "4dfacbef-46ea-49f7-8ac1-6420b1cd69bc";
+    const vo = new UniqueEntityId(uuid);
+    expect(vo.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it("should return a valid uuid", () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+    const vo = new UniqueEntityId();
+    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+});
